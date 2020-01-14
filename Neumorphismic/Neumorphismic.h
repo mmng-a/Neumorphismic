f15c2ea2cd074ed0411482c27df320671ef8dbd2
Masashi Aso <maashi.apple.japan@icloud.com>
2020-01-14 17:25:37 +0900

    Initial Commit

---
+++ Neumorphismic/Neumorphismic.h
@@ -0,0 +1,19 @@
+//
+//  Neumorphismic.h
+//  Neumorphismic
+//
+//  Created by Masashi Aso on 2020/01/14.
+//  Copyright © 2020 Masashi Aso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Neumorphismic.
+FOUNDATION_EXPORT double NeumorphismicVersionNumber;
+
+//! Project version string for Neumorphismic.
+FOUNDATION_EXPORT const unsigned char NeumorphismicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Neumorphismic/PublicHeader.h>
+
+
